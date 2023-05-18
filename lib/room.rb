38b97f00f0361b8fa658c4693350a1348a9857f2
Captainlearyo <clayoleary@gmail.com>
2023-05-18 08:35:36 -0700

    complete Iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,20 @@
 class Room
-
-  def initialize
+  attr_reader :category, :length, :width, :is_painted
+  #attr__accessor :
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width.to_i
+    @is_painted = false
 
   end
+
+  def area 
+    @length * @width
+  end
+
+  def paint
+    @is_painted = true
+  end
 end
+
